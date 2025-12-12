a79bb8b15450dfef294393e32040c5a862408ca4
Ramashery <54557810+Ramashery@users.noreply.github.com>
2025-12-12 07:02:41 +0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -9,7 +9,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyBflzOWVf3HgDpdUhha3qvyeUJf7i6dOuk",
   authDomain: "wine-91d0e.firebaseapp.com",
   projectId: "wine-91d0e",
-  storageBucket: "wine-91d0e.appspot.com", // <-- Я исправил опечатку здесь, должно быть .appspot.com
+  // ВОТ ИСПРАВЛЕННАЯ СТРОКА
+  storageBucket: "wine-91d0e.firebasestorage.app", 
   messagingSenderId: "1021620433427",
   appId: "1:1021620433427:web:5439252fb350c4455a85e6",
   measurementId: "G-TRWHY3KXK1"
